0d61f851a963c3494c3112150fd760df60e2b497
Code Wizards <94300419+CodeWizardsDev@users.noreply.github.com>
2023-09-14 21:59:25 +0330

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mohammad_Hs5'
+description 'Wizard Development Team Vehicle Showroom Script (QB-Core)'
+version '1.0.0'
+
+dependency 'qb-core'
+
+shared_script {
+    'config.lua',
+}
+
+client_scripts {
+    'client.lua'
+}
+
+lua54 'yes'
