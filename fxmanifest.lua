b6fb5ded5c7f27bef8968668266218267fe63b14
Code Wizards <94300419+CodeWizardsDev@users.noreply.github.com>
2023-09-15 20:33:28 +0330

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,7 @@ game 'gta5'
 
 author 'Mohammad_Hs5'
 description 'Wizard Development Team Vehicle Showroom Script (QB-Core)'
-version '1.0.0'
-
-dependency 'qb-core'
+version '1.1.0'
 
 shared_script {
     'config.lua',
